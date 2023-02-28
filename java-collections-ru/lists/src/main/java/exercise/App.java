e346bac9aa376e393ffde7a39ff29ca73feb5741
@hexlet/cli <support@hexlet.io>
2023-02-28 12:55:04 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -13,11 +13,11 @@ public class App {
         List<String> listOfLetters = new ArrayList<>();
 
         for (int k = 0; k < stringLength; k++) {
-            listOfLetters.add((String.valueOf(symbolsForWords.charAt(k))).toLowerCase()); // можно ли так поментяь регистр?
+            listOfLetters.add((String.valueOf(symbolsForWords.charAt(k))).toLowerCase());
         }
 
         for (int i = 0; i < wordLength; i++) {
-            String currentLetterOfWord = (String.valueOf(word.charAt(i))).toLowerCase(); // можно ли так сделать? Либо в другом месте привести к нижнему регистру?
+            String currentLetterOfWord = (String.valueOf(word.charAt(i))).toLowerCase();
             if (listOfLetters.indexOf(currentLetterOfWord) != -1) {
                 listOfLetters.remove(currentLetterOfWord);
             } else {

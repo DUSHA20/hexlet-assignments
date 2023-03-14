e3270525686cefd0a14efd9581ff529955dec85e
@hexlet/cli <support@hexlet.io>
2023-03-14 22:50:38 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -10,10 +10,10 @@ public class App {
     public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
         int sizeOfList = books.size();
         int countOfConcidences = 0;
-        List<Integer> matchIndices = new ArrayList<>(); // список индексов совпадений
+        List<Map<String, String>> result = new ArrayList<>();
 
         for (int i = 0; i < sizeOfList; i++) {
-            List<String> val1 = new ArrayList<>((books.get(i)).values());
+            ArrayList<String> val1 = new ArrayList<>((books.get(i)).values());
             List<String> val2 = new ArrayList<>(where.values());
             int sizeOfListValues2 = val2.size();
 
@@ -24,21 +24,16 @@ public class App {
                 }
             }
 
-            if (countOfConcidences != sizeOfListValues2) {
-                matchIndices.add(i);
+            if (countOfConcidences == sizeOfListValues2) {
+                result.add(books.get(i));
             }
             countOfConcidences = 0;
         }
-        int sizeOfmatchIndices = matchIndices.size();
-        for (int j = sizeOfmatchIndices - 1; j >= 0; j--) {
-            int matchIndex = matchIndices.get(j);
-            books.remove(matchIndex);
-        }
 
-        if (books.isEmpty()) {
+        if (result.isEmpty()) {
             return new ArrayList<>();
         } else {
-            return books;
+            return result;
         }
     }
 }

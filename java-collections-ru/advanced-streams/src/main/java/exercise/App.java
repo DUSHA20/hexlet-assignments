fe1fa29fadb78be9a505b65192d8ed3a4fe5a668
@hexlet/cli <support@hexlet.io>
2023-04-15 13:55:52 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -5,31 +5,17 @@ import java.util.Arrays;
 
 // BEGIN
 class App {
-    public static String getForwardedVariables(String environmentVariables) {
-        char quotes = '"';
-        String indexOfquotes = "";
-        for (int i = 0; i < environmentVariables.length(); i++) {
-            if (environmentVariables.charAt(i) == quotes) {
-                indexOfquotes = indexOfquotes + i + " ";
-            }
-        }
-        String resultLine = "";
-        String wordsInQuotes = "";
-        String[] arrayOfIndex = indexOfquotes.split(" ");
-        for (int j = 0; j < arrayOfIndex.length - 1; j += 2) {
-            wordsInQuotes = environmentVariables.substring(Integer.parseInt(arrayOfIndex[j]) + 1, Integer.parseInt(arrayOfIndex[j + 1]));
-            String[] rightWords = wordsInQuotes.split(",");
-            for (int k = 0; k < rightWords.length; k++) {
-                if (rightWords[k].length() < 12)
-                    continue;
-                if((rightWords[k].substring(0,12)).equals("X_FORWARDED_")) {
-                    resultLine = resultLine + rightWords[k].substring(12) + ",";
-                }
-            }
-        }
-        int endIndex = resultLine.length();
-        String finalResult = resultLine.substring(0, endIndex - 1);
-        return finalResult;
+    public static String getForwardedVariables(String config) {
+        String[] lines = config.split("\n");
+        return Arrays.stream(lines)
+                .filter(line -> line.startsWith("environment="))
+                .map(line -> line.replaceAll("environment=", ""))
+                .map(line -> line.replaceAll("\"", ""))
+                .map(line -> line.split(","))
+                .flatMap(Arrays::stream)
+                .filter(kv -> kv.startsWith("X_FORWARDED_"))
+                .map(kv -> kv.replaceFirst("X_FORWARDED_", ""))
+                .collect(Collectors.joining(","));
     }
 }
 //END

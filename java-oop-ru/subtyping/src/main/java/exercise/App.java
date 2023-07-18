d6482a321422a1c20bcb19ca34a8f613d76f6c75
@hexlet/cli <support@hexlet.io>
2023-07-18 21:10:11 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -7,11 +7,15 @@ import java.util.Set;
 // BEGIN
 public class App implements KeyValueStorage {
 
+    private Map<String, String> database;
+
+    public App(Map<String, String> initialData) {
+        this.database = new HashMap<>(initialData);
+    }
+
     public static void swapKeyValue(KeyValueStorage storage) {
-
         Map<String, String> database = storage.toMap();
-
-        Map<String, String> swappedDatabase = new HashMap<String, String>();
+        Map<String, String> swappedDatabase = new HashMap<>();
 
         for (Map.Entry<String, String> entry : database.entrySet()) {
             swappedDatabase.put(entry.getValue(), entry.getKey());
@@ -19,26 +23,26 @@ public class App implements KeyValueStorage {
 
         database.clear();
         database.putAll(swappedDatabase);
+    }
 
-        @Override
-        public void set(String key, String value) {
-            database.put(key, value);
-        }
+    @Override
+    public void set(String key, String value) {
+        database.put(key, value);
+    }
 
-        @Override
-        public void unset(String key) {
-            database.remove(key);
-        }
+    @Override
+    public void unset(String key) {
+        database.remove(key);
+    }
 
-        @Override
-        public String get(String key, String defaultValue) {
-            return database.getOrDefault(key, defaultValue);
-        }
+    @Override
+    public String get(String key, String defaultValue) {
+        return database.getOrDefault(key, defaultValue);
+    }
 
-        @Override
-        public Map<String, String> toMap() {
-            return new HashMap<>(database);
-        }
+    @Override
+    public Map<String, String> toMap() {
+        return new HashMap<>(database);
     }
 }
 // END

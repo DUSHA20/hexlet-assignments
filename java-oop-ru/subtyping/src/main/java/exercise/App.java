9ab88633085e21cf1554e03a013be4bd7243b529
@hexlet/cli <support@hexlet.io>
2023-07-18 22:19:17 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -5,46 +5,16 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
-public class App implements KeyValueStorage {
-
-    // Публичный статический метод для обмена ключей и значений в объекте базы данных
+public class App {
     public static void swapKeyValue(KeyValueStorage storage) {
-        String[] keys = new String[storage.toMap().size()];
-        String[] values = new String[storage.toMap().size()];
-        int index = 0;
+        Map<String, String> data = storage.toMap();
 
-        // Сохраняем ключи и значения в отдельные массивы
-        for (Entry<String, String> entry : storage.toMap().entrySet()) {
-            keys[index] = entry.getKey();
-            values[index] = entry.getValue();
-            index++;
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            storage.unset(key);
+            storage.set(value, key);
         }
-
-        // Очищаем исходное хранилище и добавляем обмененные значения
-        storage.clear();
-        for (int i = 0; i < keys.length; i++) {
-            storage.set(values[i], keys[i]);
-        }
-    }
-
-    @Override
-    public void set(String key, String value) {
-        database.put(key, value);
-    }
-
-    @Override
-    public void unset(String key) {
-        database.remove(key);
-    }
-
-    @Override
-    public String get(String key, String defaultValue) {
-        return database.getOrDefault(key, defaultValue);
-    }
-
-    @Override
-    public Map<String, String> toMap() {
-        return new HashMap<String, String>(database);
     }
 }
 // END

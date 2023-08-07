97607cd816b43da5c2ce15f3824390ae80c82bd0
@hexlet/cli <support@hexlet.io>
2023-08-07 12:04:26 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/Tag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -4,27 +4,6 @@ import java.util.stream.Collectors;
 import java.util.Map;
 
 // BEGIN
-//public class Tag {
-//    private String name;
-//    private String content;
-//
-//    public Tag(String name, String content) {
-//        this.name = name;
-//        this.content = content;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public String toString() {
-//        return "<" + name + ">" + content + "</" + name + ">";
-//    }
-//}
 public class Tag {
     private String name;
     private String content;
